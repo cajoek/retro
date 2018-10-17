db65988baa984330c14ffc5ac27175ad25cfe18b
Cajoek <cajoek92@gmail.com>
2018-10-17 15:19:15 +0200

    Integrated mario cart, step 1

---
+++ retro/data/stable/SuperMarioKart-Snes/script.lua
@@ -0,0 +1,34 @@
+p1_previous_progress = data.p1_progress
+p2_previous_progress = data.p2_progress
+function get_reward()
+    local p1_reward = sign(data.p1_progress - p1_previous_progress)
+    local p2_reward = sign(data.p2_progress - p2_previous_progress)
+    p1_previous_progress = data.p1_progress
+    p2_previous_progress = data.p2_progress
+    if p1_reward > 0 then
+        p1_reward = p1_reward/(data.p1_placing/2 + 1)
+    end
+    if p2_reward > 0 then
+        p2_reward = p2_reward/(data.p2_placing/2 + 1)
+    end
+    return {p1_reward, p2_reward}
+    --return p1_reward
+end
+    
+
+function done_check()
+    if data.finished == 0 then
+        return false
+    end
+    return true
+end
+
+function sign(x)
+    if x<0 then
+      return -1
+    elseif x>0 then
+      return 1
+    else
+      return 0
+    end
+ end
