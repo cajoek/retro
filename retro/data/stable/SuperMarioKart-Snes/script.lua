028c13e0e9ea3d73fb5f6a246a0cba855310912a
cajoek <cajoek92@gmail.com>
2018-10-31 12:10:13 +0000

    New reward function
    Reward is only recived when absolute progress is made

--- retro/data/stable/SuperMarioKart-Snes/script.lua
+++ retro/data/stable/SuperMarioKart-Snes/script.lua
@@ -1,32 +1,27 @@
-p1_previous_progress = data.p1_progress
+TIMEOUT = 60*15 -- 15s timeout
+
+max_progress = data.p1_progress
+previous_progress = data.p1_progress
+progress_timer = TIMEOUT
 function get_reward()
-    local p1_reward = sign(data.p1_progress - p1_previous_progress)
-    p1_previous_progress = data.p1_progress
-    p1_reward = p1_reward/(data.p1_placing/2 + 1)
-    return p1_reward
+    progress_timer = progress_timer - 1
+    if data.p1_progress > max_progress then
+        max_progress = data.p1_progress
+        previous_progress = data.p1_progress
+        progress_timer = TIMEOUT
+        return 1/(data.p1_placing/2 + 1)
+    elseif data.p1_progress < previous_progress then
+        previous_progress = data.p1_progress
+        return -1/(data.p1_placing/2 + 1)
+    end
+    previous_progress = data.p1_progress
+    return 0
 end
 
-TIMEOUT = 60*10 -- 10s timeout
-p1_max_progress = data.p1_progress
-p1_progress_timer = TIMEOUT
+
 function done_check()
-    p1_progress_timer = p1_progress_timer - 1
-    if data.p1_progress > p1_max_progress then
-        p1_max_progress = data.p1_progress
-        p1_progress_timer = TIMEOUT
-    end
-    if data.finished ~= 0 or p1_progress_timer <= 0 then
+    if data.finished ~= 0 or progress_timer <= 0 then
         return true
     end
     return false
 end
-
-function sign(x)
-    if x<0 then
-      return -1
-    elseif x>0 then
-      return 1
-    else
-      return 0
-    end
- end
